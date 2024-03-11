85e756625c15c7c889136eb537fe45ee99e753d8
Pushkar Singh <84075300+PushkarOP@users.noreply.github.com>
2024-03-11 20:13:45 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template_string, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import os
+import string
+import random
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'  # Use your own database URI
+db = SQLAlchemy(app)
+
+class Text(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    content = db.Column(db.Text)
+
+@app.route('/', methods=['POST'])
+def post_text():
+    content = request.get_json().get('content')
+    id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+    text = Text(id=id, content=content)
+    db.session.add(text)
+    db.session.commit()
+    return jsonify({'id': id, 'link': request.host_url + 'text/' + id})
+
+@app.route('/text/<id>', methods=['GET'])
+def get_text(id):
+    text = Text.query.get_or_404(id)
+    return render_template_string('<html><body><pre>' + text.content + '</pre></body></html>')
+
+if __name__ == '__main__':
+    if not os.path.exists('/tmp/test.db'):
+        db.create_all()
+    app.run(host='0.0.0.0', port=8000)
